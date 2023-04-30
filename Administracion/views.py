e6eff9d31266b3ec0dcb84befe471610d960e87c
larahoffman <larahoff02@gmail.com>
2023-04-30 00:06:54 -0300

    formulario - incompleto

--- Administracion/views.py
+++ Administracion/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import *
 # Create your views here.
 
 def inicio(request):
     return render(request, 'Administracion/index.html')
 
 def clientes(request):
+    if request.method == 'post':
+        cliente = Clientes(request.POST['nombre'], request.POST['correo'], int(request.POST['telefono'])) # deberia pasarle el id?
+        cliente.save()
+        return render(request, "Administracion/index.html")
     return render(request, 'Administracion/clientes.html')
 
 def productos(request):
