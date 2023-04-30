0537aa4ece49909f6d400b295d40a2941b6424cd
larahoffman <larahoff02@gmail.com>
2023-04-29 23:43:47 -0300

    agregado modelos al panel admin

--- Administracion/admin.py
+++ Administracion/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Clientes)
+
+admin.site.register(Productos)
+
+admin.site.register(Pedidos)
