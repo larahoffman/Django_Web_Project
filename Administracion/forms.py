4dbeb9da3990ff81d97ebefb584097fd1722e1fa
larahoffman <larahoff02@gmail.com>
2023-05-01 19:47:38 -0300

    cambios formularios

--- Administracion/forms.py
+++ Administracion/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 
 class ClientesFormulario(forms.Form):
-    nombre = forms.CharField()
-    correo = forms.EmailField()
-    telefono = forms.IntegerField()
+    nombre = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    correo = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    telefono = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control"}))
+
+class ProductosFormulario(forms.Form):
+    descripcion = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    codigo_producto = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control"}))
+
+class PedidosFormulario(forms.Form):
+    numero_pedido = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control"}))
+    ESTADOS_OPCIONES = [("pendiente", "pendiente"), ("entregado", "entregado")]
+    estado = forms.ChoiceField(widget=forms.Select(attrs={"class": "form-select"}), choices=ESTADOS_OPCIONES)
+    notas = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
--- Administracion/views.py
+++ Administracion/views.py
@@ -23,15 +23,46 @@ def clientes(request):
 
         if miFormulario.is_valid:
             info = miFormulario.cleaned_data
-            cliente = Clientes(nombre = info['nombre'], correo = info['correo'], telefono = int(info['telefono']))
+            cliente = Clientes(nombre = info['nombre'], correo = info['correo'], telefono = info['telefono'])
             cliente.save()
-            return render(request, "Administracion/index.html")
+
+            mensaje = "¡Cliente agregado con éxito!"
+            miFormulario = ClientesFormulario() #limpio los datos
+            return render(request, "Administracion/clientes.html", {"miFormulario":miFormulario, "mensaje":mensaje})
     else:
         miFormulario = ClientesFormulario()
         return render(request, "Administracion/clientes.html", {"miFormulario":miFormulario})
 
 def productos(request):
-    return render(request, 'Administracion/productos.html')
+    if request.method == 'POST':
+        miFormulario = ProductosFormulario(request.POST)
+        print(miFormulario)
+
+        if miFormulario.is_valid:
+            info = miFormulario.cleaned_data
+            Producto = Productos(descripcion = info['descripcion'], codigo_producto = info['codigo_producto'])
+            Producto.save()
+
+            mensaje = "¡Producto agregado con éxito!"
+            miFormulario = ProductosFormulario()
+            return render(request, "Administracion/productos.html", {"miFormulario":miFormulario, "mensaje":mensaje})
+    else:
+        miFormulario = ProductosFormulario()
+        return render(request, "Administracion/productos.html", {"miFormulario":miFormulario})        
 
 def pedidos(request):
-    return render(request, 'Administracion/pedidos.html')
+    if request.method == 'POST':
+        miFormulario = PedidosFormulario(request.POST)
+        print(miFormulario)
+
+        if miFormulario.is_valid:
+            info = miFormulario.cleaned_data
+            pedido = Pedidos(numero_pedido = info['numero_pedido'], estado = info['estado'], notas = info['notas'])
+            pedido.save()
+
+            mensaje = "¡Pedido agregado con éxito!"
+            miFormulario = PedidosFormulario()
+            return render(request, "Administracion/pedidos.html", {"miFormulario":miFormulario, "mensaje":mensaje})
+    else:
+        miFormulario = PedidosFormulario()
+        return render(request, "Administracion/pedidos.html", {"miFormulario":miFormulario})
