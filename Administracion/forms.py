b842e65afdec32fe7ab82f71be41fb41602df771
larahoffman <larahoff02@gmail.com>
2023-05-01 21:00:25 -0300

    notas ya no es requerido

--- Administracion/forms.py
+++ Administracion/forms.py
@@ -13,4 +13,4 @@ class PedidosFormulario(forms.Form):
     numero_pedido = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control"}))
     ESTADOS_OPCIONES = [("pendiente", "pendiente"), ("entregado", "entregado")]
     estado = forms.ChoiceField(widget=forms.Select(attrs={"class": "form-select"}), choices=ESTADOS_OPCIONES)
-    notas = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    notas = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}), required=False)
