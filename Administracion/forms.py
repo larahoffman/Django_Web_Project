52f7e3b1e8f9a9d2cf023032c518cb27f6d17690
larahoffman <larahoff02@gmail.com>
2023-05-01 21:46:48 -0300

    ultimos detalles estilos

--- Administracion/forms.py
+++ Administracion/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 
 class ClientesFormulario(forms.Form):
-    nombre = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
-    correo = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
-    telefono = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control"}))
+    nombre = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Ingrese un nombre"}))
+    correo = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control", "placeholder": "Ingrese un correo"}))
+    telefono = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "Ingrese un telefono"}))
 
 class ProductosFormulario(forms.Form):
-    descripcion = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
-    codigo_producto = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control"}))
+    descripcion = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Ingrese una descripcion del producto"}))
+    codigo_producto = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "Ingrese el codigo del producto. Ejemplo: 334588"}))
 
 class PedidosFormulario(forms.Form):
-    numero_pedido = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control"}))
+    numero_pedido = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "Ingrese el numero de pedido"}))
     ESTADOS_OPCIONES = [("pendiente", "pendiente"), ("entregado", "entregado")]
     estado = forms.ChoiceField(widget=forms.Select(attrs={"class": "form-select"}), choices=ESTADOS_OPCIONES)
-    notas = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}), required=False)
+    notas = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Ingrese una nota adicional"}), required=False)
