15e3efcbadf2a11d7381812ce0638991dcb2ba1a
larahoffman <larahoff02@gmail.com>
2023-05-02 20:43:04 -0300

    main path lleva a la app principal administracion

--- Tercera_preentrega_Hoffman/urls.py
+++ Tercera_preentrega_Hoffman/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Administracion/', include('Administracion.urls'))
+    path('', include('Administracion.urls'))
 ]
